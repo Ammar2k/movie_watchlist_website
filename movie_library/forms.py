d9aa0bf5ac1a4a614592915b214be3106de26ff7
Ammar <muhammad2000ammar@gmail.com>
2023-05-21 13:29:10 +0500

    first commit

---
+++ movie_library/forms.py
@@ -0,0 +1,85 @@
+from flask_wtf import FlaskForm  # protects from csrf attacks
+from wtforms import (
+    IntegerField,
+    PasswordField,
+    StringField,
+    SubmitField,
+    TextAreaField,
+    URLField,
+)
+from wtforms.validators import Email, EqualTo, InputRequired, Length, NumberRange
+
+
+class MovieForm(FlaskForm):
+    title = StringField("Title", validators=[InputRequired()])
+    director = StringField("Director", validators=[InputRequired()])
+
+    year = IntegerField(
+        "Year",
+        validators=[
+            InputRequired(),
+            NumberRange(
+                min=1930,
+                max=2023,
+                message="Please enter a valid year",
+            ),
+        ],
+    )
+
+    submit = SubmitField("Add Movie")
+
+
+class StringListField(TextAreaField):
+    def _value(self):
+        if self.data:
+            return "\n".join(self.data)
+        else:
+            return ""
+
+    def process_formdata(self, valuelist):
+        if valuelist and valuelist[0]:
+            self.data = [line.strip() for line in valuelist[0].split("\n")]
+        else:
+            self.data = []
+
+
+class ExtendedMovieForm(MovieForm):
+    cast = StringListField("Cast")
+    series = StringListField("Series")
+    tags = StringListField("Tags")
+    description = TextAreaField("Description")
+    video_link = URLField("Video Link")
+
+    submit = SubmitField("Submit")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField(
+        "Password",
+        validators=[
+            InputRequired(),
+            Length(
+                min=4,
+                max=20,
+                message="Your password must be between 4 and 20 characters long.",
+            ),
+        ],
+    )
+    confirm_password = PasswordField(
+        "Confirm password",
+        validators=[
+            InputRequired(),
+            EqualTo(
+                "password",
+                message="This password did not match the one in the password field.",
+            ),
+        ],
+    )
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
